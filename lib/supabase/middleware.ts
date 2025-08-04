0beed0d01fbec71ee15879bf0be692e6c807b687
devXamr <devxamr@gmail.com>
2025-08-04 15:57:17 +0530

    feat: added logo url to allow unauthorized access

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -50,8 +50,10 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/my-widget.js")
-  ) {
+    !request.nextUrl.pathname.startsWith("/my-widget.js") &&
+    !request.nextUrl.pathname.startsWith("/convertdesklogo.png")
+
+) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/auth/login";
